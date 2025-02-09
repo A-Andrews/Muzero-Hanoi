7a6b899cd351d778467157d10324889a5f147e0d
Austin Andrews <austinandrews6@gmail.com>
2025-02-09 19:51:06 +0000

    Fix mistakes

--- MCTS/mcts.py
+++ MCTS/mcts.py
@@ -1,10 +1,12 @@
-import torch
 import numpy as np
+import torch
+
 from MCTS.node import Node
 from MCTS.utils_mcts import MinMaxStats
 
-class MCTS():
-    """ Define class to run MCTS"""
+
+class MCTS:
+    """Define class to run MCTS"""
 
     def __init__(
         self,
@@ -15,8 +17,8 @@ class MCTS():
         device,
         h_dim=64,
         clip_grad=True,
-        root_exploration_eps = 0.25,
-        known_bounds = [],
+        root_exploration_eps=0.25,
+        known_bounds=[],
     ):
         self.min_max_stats = MinMaxStats()
         self.pb_c_base = 19652
@@ -28,9 +30,9 @@ class MCTS():
         self.n_simulations = n_simulations
         self.batch_s = batch_s
         self.dev = device
-            
-    def run_mcts(self,state, network, temperature, deterministic):           
-        """ Run MCT
+
+    def run_mcts(self, state, network, temperature, deterministic):
+        """Run MCT
         Args:
             state: current obs from the environment.
             network: MuZeron network instance
@@ -44,48 +46,70 @@ class MCTS():
         """
 
         # Create root node
-        state = torch.from_numpy(state).to(self.dev,dtype=torch.float32)
+        state = torch.from_numpy(state).to(self.dev, dtype=torch.float32)
         h_state, rwd, pi_probs, value = network.initial_inference(state)
         prior_prob = pi_probs
-        root_node = Node(prior=0.0) # the root node does not have prior probs since it is the root
+        root_node = Node(
+            prior=0.0
+        )  # the root node does not have prior probs since it is the root
 
         # Add dirichlet noise to the prior probabilities to root node.
-        if not deterministic and self.root_dirichlet_alpha > 0.0 and self.root_exploration_eps > 0.0:
-            prior_prob = self.add_dirichlet_noise(prior_prob, eps=self.root_exploration_eps, alpha=self.root_dirichlet_alpha)
-        
+        if (
+            not deterministic
+            and self.root_dirichlet_alpha > 0.0
+            and self.root_exploration_eps > 0.0
+        ):
+            prior_prob = self.add_dirichlet_noise(
+                prior_prob,
+                eps=self.root_exploration_eps,
+                alpha=self.root_dirichlet_alpha,
+            )
+
         # fill node with data and add "children actions", by expanding
-        root_node.expand(prior_prob,h_state, rwd) 
+        root_node.expand(prior_prob, h_state, rwd)
 
         for s in range(self.n_simulations):
-            #print('Simulation n. :', s+1)
+            # print('Simulation n. :', s+1)
             ## ====  Phase 1 - Select ====
             # Reset initial node to root for each mcts simulation
-            node = root_node 
+            node = root_node
 
             # Select best child node until reach a leaf
             # NOTE: the leaf will not be expanded (i.e. have no state)
-            self.latent_actions = [] # Store only the actions for the last simulation
+            self.latent_actions = []  # Store only the actions for the last simulation
             while node.is_expanded:
-                node = node.best_child(self, self.min_max_stats) # pass MCTS object to have access to the config
-                self.latent_actions.append(torch.tensor([node.move], dtype=torch.long, device=self.dev))
-                #print('Move: ', node.move,"\n")
+                node = node.best_child(
+                    self, self.min_max_stats
+                )  # pass MCTS object to have access to the config
+                self.latent_actions.append(
+                    torch.tensor([node.move], dtype=torch.long, device=self.dev)
+                )
+                # print('Move: ', node.move,"\n")
 
-            ## ==== Phase 2 - Expand leaf - based on parent state and action associated to that (best) leaf ==== 
-            h_state = torch.from_numpy(node.parent.h_state).to(self.dev,dtype=torch.float32) # node.parent because while loop ends at not expanded (best) child
+            ## ==== Phase 2 - Expand leaf - based on parent state and action associated to that (best) leaf ====
+            h_state = torch.from_numpy(node.parent.h_state).to(
+                self.dev, dtype=torch.float32
+            )  # node.parent because while loop ends at not expanded (best) child
             action = torch.tensor([node.move], dtype=torch.long, device=self.dev)
 
             # Convert action to 1-hot encoding
-            action = torch.nn.functional.one_hot(action, num_classes=network.num_actions).squeeze().to(self.dev,dtype=torch.float32)
+            action = (
+                torch.nn.functional.one_hot(action, num_classes=network.num_actions)
+                .squeeze()
+                .to(self.dev, dtype=torch.float32)
+            )
 
             # Take a step in latent space
-            h_state, rwd, pi_probs, value = network.recurrent_inference(h_state, action) # compute latent state for best action (child)
+            h_state, rwd, pi_probs, value = network.recurrent_inference(
+                h_state, action
+            )  # compute latent state for best action (child)
 
-            #node.expand(prior_prob, h_state, rwd) #NOTE: I don't understand prior prob here, shouldn't come from a pi_probs ?
-            node.expand(pi_probs, h_state, rwd) #NOTE: Trial using pi_probs!!!
+            # node.expand(prior_prob, h_state, rwd) #NOTE: I don't understand prior prob here, shouldn't come from a pi_probs ?
+            node.expand(pi_probs, h_state, rwd)  # NOTE: Trial using pi_probs!!!
 
             ## ==== Phase 3 - Backup on leaf node ====
             node.backup(value, self, self.min_max_stats)
-        
+
         # Play: generate action prob from the root node to be played in the env.
         child_visits = root_node.child_N
         pi_prob = self.generate_play_policy(child_visits, temperature)
@@ -97,14 +121,14 @@ class MCTS():
             # Sample a action.
             action_idx = np.random.choice(np.arange(pi_prob.shape[0]), p=pi_prob)
 
-        action = root_node.children[action_idx].move        
+        action = root_node.children[action_idx].move
 
         # pi_prob and root_node.Q are returned to be stored for the training phase
         # root_node.Q is only needed if use TD-returns, by bootstrapping value of future (root) states to update values of current (root) state
         return action, pi_prob, root_node.Q
 
     def return_latent_actions(self):
-        """ Return the latent actions driving the last sweep of the MCTS """
+        """Return the latent actions driving the last sweep of the MCTS"""
         return self.latent_actions
 
     def add_dirichlet_noise(self, prob, eps=0.25, alpha=0.25):
@@ -116,8 +140,11 @@ class MCTS():
 
         Returns:
             action probabilities with added dirichlet noise.
-        """    
-        if not isinstance(prob, np.ndarray) or prob.dtype not in (np.float32, np.float64):
+        """
+        if not isinstance(prob, np.ndarray) or prob.dtype not in (
+            np.float32,
+            np.float64,
+        ):
             raise ValueError(f"Expect `prob` to be a numpy.array, got {prob}")
 
         alphas = np.ones_like(prob) * alpha
@@ -125,25 +152,27 @@ class MCTS():
         noised_prob = (1 - eps) * prob + eps * noise
 
         return noised_prob
-                    
-    def generate_play_policy(self,visits_count, temperature):
-        """ Returns policy action probabilities proportional to their exponentiated visit count during MCTS 
+
+    def generate_play_policy(self, visits_count, temperature):
+        """Returns policy action probabilities proportional to their exponentiated visit count during MCTS
         Args:
             visits_count: a 1D numpy.array contains child node visits count.
             temperature: a parameter controls the level of exploration.
-        Returns:        
+        Returns:
             a 1D numpy.array contating the action prob for real env
         """
 
         if not 0.0 <= temperature <= 1.0:
-            raise ValueError(f"Expect `temperature` to be in the range [0.0, 1.0], got {temperature}")
+            raise ValueError(
+                f"Expect `temperature` to be in the range [0.0, 1.0], got {temperature}"
+            )
 
-        visits_count = np.asarray(visits_count,dtype=np.int64)
+        visits_count = np.asarray(visits_count, dtype=np.int64)
 
         if temperature > 0.0:
             # limit the exponent in the range of [1.0, 5.0]
             # to avoid overflow when doing power operation over large numbers
-            exp = max(1.0,min(5.0, 1.0/temperature))
-            visits_count = np.power(visits_count,exp)
+            exp = max(1.0, min(5.0, 1.0 / temperature))
+            visits_count = np.power(visits_count, exp)
 
         return visits_count / np.sum(visits_count)
--- networks.py
+++ networks.py
@@ -7,8 +7,9 @@ import torch.optim as opt
 # NOTE: The original code uses an additional transformation from policy, reward and value logits (i.e. value_suppot_vector ect.) to the actual polcy, value etc.
 # in the current implementation, we skip this additional transform. and map directly onto the values.
 
+
 class MuZeroNet(nn.Module):
-    """ Class containing all MuZero nets"""
+    """Class containing all MuZero nets"""
 
     def __init__(
         self,
@@ -16,8 +17,8 @@ class MuZeroNet(nn.Module):
         action_s,
         lr,
         device,
-        reward_s = 1,
-        h1_s = 256, # 64 seems to work worse!
+        reward_s=1,
+        h1_s=256,  # 64 seems to work worse!
         reprs_output_size=64,
         weight_decay=1e-4,
         TD_return=False,
@@ -29,14 +30,14 @@ class MuZeroNet(nn.Module):
         self.TD_return = TD_return
         self.reprs_output_size = reprs_output_size
 
-        #NOTE: currently use support for both value and rwd prediction 
+        # NOTE: currently use support for both value and rwd prediction
         if TD_return:
-            self.support_size=33
-        else:        
-            self.support_size=1
+            self.support_size = 33
+        else:
+            self.support_size = 1
 
         self.representation_net = nn.Sequential(
-            nn.Linear(rpr_input_s,h1_s),
+            nn.Linear(rpr_input_s, h1_s),
             nn.ReLU(),
             nn.Linear(h1_s, reprs_output_size),
         )
@@ -50,7 +51,7 @@ class MuZeroNet(nn.Module):
         self.rwd_net = nn.Sequential(
             nn.Linear(reprs_output_size, h1_s),
             nn.ReLU(),
-            nn.Linear(h1_s, self.support_size)
+            nn.Linear(h1_s, self.support_size),
         )
 
         self.policy_net = nn.Sequential(
@@ -65,12 +66,13 @@ class MuZeroNet(nn.Module):
             nn.Linear(h1_s, self.support_size),
         )
 
-        self.optimiser = opt.Adam(self.parameters(),lr)#, weight_decay=weight_decay)
+        self.optimiser = opt.Adam(self.parameters(), lr)  # , weight_decay=weight_decay)
 
     @torch.no_grad()
-    def initial_inference(self,x):
+    def initial_inference(self, x):
         """During self-play, given environment observation, use representation function to predict initial hidden state.
-        Then use prediction function to predict policy probabilities and state value (on the hidden state)."""
+        Then use prediction function to predict policy probabilities and state value (on the hidden state).
+        """
 
         # Representation
         h_state = self.represent(x)
@@ -78,9 +80,11 @@ class MuZeroNet(nn.Module):
         # Prediction
         pi_logits, value = self.prediction(h_state)
 
-        pi_probs = F.softmax(pi_logits,dim=-1) # NOTE: dim ?
+        pi_probs = F.softmax(pi_logits, dim=-1)  # NOTE: dim ?
 
-        rwd = torch.zeros_like(value) # NOTE: Not sure why it doesn't predict rwd for initial inference
+        rwd = torch.zeros_like(
+            value
+        )  # NOTE: Not sure why it doesn't predict rwd for initial inference
 
         pi_probs = pi_probs.squeeze(0).cpu().numpy()
         value = value.squeeze(0).cpu().item()
@@ -88,20 +92,21 @@ class MuZeroNet(nn.Module):
         h_state = h_state.squeeze(0).cpu().numpy()
 
         return h_state, rwd, pi_probs, value
-        
+
     @torch.no_grad()
     def recurrent_inference(self, h_state, action):
         """During self-play, given hidden state at timestep `t-1` and action at t,
         use dynamics function to predict the reward and next hidden state,
-        and use prediction function to predict policy probabilities and state value (on new hidden state)."""
+        and use prediction function to predict policy probabilities and state value (on new hidden state).
+        """
 
-        # Dynamic 
+        # Dynamic
         h_state, rwd = self.dynamics(h_state, action)
 
         # Prediction
         pi_logits, value = self.prediction(h_state)
 
-        pi_probs = F.softmax(pi_logits,dim=-1) # NOTE: dim ?
+        pi_probs = F.softmax(pi_logits, dim=-1)  # NOTE: dim ?
 
         pi_probs = pi_probs.squeeze(0).cpu().numpy()
         value = value.squeeze(0).cpu().item()
@@ -111,19 +116,19 @@ class MuZeroNet(nn.Module):
         return h_state, rwd, pi_probs, value
 
     def update(self, loss):
-        
+
         self.optimiser.zero_grad()
         loss.backward()
         self.optimiser.step()
 
-    def represent(self,x):
-        h_state = self.representation_net(x)
+    def represent(self, x):
+        h_state = self.representation_net(x.cpu())
         norm_h_state = self.normalize_h_state(h_state)
         return norm_h_state
 
     def dynamics(self, h_state, action):
-        x = torch.cat([h_state,action],dim=-1)
-        new_h_state = self.dynamic_net(x)
+        x = torch.cat([h_state, action], dim=-1)
+        new_h_state = self.dynamic_net(x.cpu())
         rwd_prediction = self.rwd_net(new_h_state)
 
         if self.TD_return:
@@ -158,8 +163,8 @@ class MuZeroNet(nn.Module):
         Returns:
             a 2D tensor which represent the transformed expected value, shape [B, 1].
         """
-        max_value = (self.support_size -1) // 2
-        min_value = - max_value
+        max_value = (self.support_size - 1) // 2
+        min_value = -max_value
 
         # Compute expected scalar
         probs = torch.softmax(logits, dim=-1)
@@ -171,7 +176,9 @@ class MuZeroNet(nn.Module):
 
     def _transform_from_2hot(self, probs, min_value, max_value):
         """Transforms from a categorical distribution to a scalar."""
-        support_space = torch.linspace(min_value,max_value, self.support_size, device=self.dev)
+        support_space = torch.linspace(
+            min_value, max_value, self.support_size, device=self.dev
+        )
         support_space = support_space.expand_as(probs)
         scalar = torch.sum(probs * support_space, dim=-1, keepdim=True)
         return scalar
@@ -184,7 +191,9 @@ class MuZeroNet(nn.Module):
     def normalize_h_state(self, h_state):
         _min = h_state.min(dim=-1, keepdim=True)[0]
         _max = h_state.max(dim=-1, keepdim=True)[0]
-        return (h_state - _min) / (_max - _min + 1e-8) ## Add small constant to avoid division by 0
+        return (h_state - _min) / (
+            _max - _min + 1e-8
+        )  ## Add small constant to avoid division by 0
 
     def set_pol_pertubation(self, pertub_magnitude):
         self.perturb_p_magnitude = pertub_magnitude
@@ -192,5 +201,5 @@ class MuZeroNet(nn.Module):
     def reset_param(self, l):
         k = np.sqrt(1 / self.reprs_output_size)
         if isinstance(l, nn.Linear):
-            nn.init.uniform_(l.weight,a=-k,b=k)
-            nn.init.uniform_(l.bias,a=-k,b=k)
+            nn.init.uniform_(l.weight, a=-k, b=k)
+            nn.init.uniform_(l.bias, a=-k, b=k)
