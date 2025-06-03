e6dbad998520c72f51cde26e32a5165ca0b5b9fe
A-Andrews <44030780+A-Andrews@users.noreply.github.com>
2025-06-03 11:09:44 +0100

    Add tdstep

--- training_main.py
+++ training_main.py
@@ -93,9 +93,10 @@ def log_command_line(
     dev,
     dirichlet_alpha,
     buffer_size,
+    TD_step,
     n_disks=None,
 ):
-    command_line = f"Env: {env_p}, Training Loops: {training_loops}, Min replay size: {min_replay_size}, lr: {lr}, discount: {discount}, n. MCTS: {n_mcts_simulations}, batch size: {batch_s}, TD_return: {TD_return}, Priority Buff: {priority_replay}, device: {dev}, dirichlet_alpha: {dirichlet_alpha}, buffer_size: {buffer_size}"
+    command_line = f"Env: {env_p}, Training Loops: {training_loops}, Min replay size: {min_replay_size}, lr: {lr}, discount: {discount}, n. MCTS: {n_mcts_simulations}, batch size: {batch_s}, TD_return: {TD_return}, Priority Buff: {priority_replay}, device: {dev}, dirichlet_alpha: {dirichlet_alpha}, buffer_size: {buffer_size}, TD_step: {TD_step}"
 
     if env_p == "Hanoi":  # if hanoi also print n. of disks
         command_line += f", N. disks: {n_disks}"
@@ -199,6 +200,7 @@ if __name__ == "__main__":
         dev,
         dirichlet_alpha,
         buffer_size,
+        n_TD_step,
         n_disks=n_disks,
     )
 
